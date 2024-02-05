2bfffaf1ec451b900d21a401e83c841bda9ef6c4
Pankaj Pawar <pankaj@Pankajs-MacBook-Air.local>
2024-02-06 05:14:07 +0530

    added recursion way & iterative way both

--- binary-search/main.go
+++ binary-search/main.go
@@ -5,21 +5,50 @@ import "fmt"
 func main() {
 	var providedArray = []int{10, 20, 30, 40, 50, 60, 70, 80}
 
-	searchNumber, length, zero, one, i, mid, foundCount := 5, len(providedArray), 0, len(providedArray)-1, 0, 0, 0
+	searchNumber, zero, one, foundCount := 60, 0, len(providedArray)-1, 0
+
+	// foundCount = binarySearchRecursive(providedArray, searchNumber, zero, one)
+	foundCount = binarySearchIterative(providedArray, searchNumber, zero, one)
+
+	if foundCount != -1 {
+		fmt.Printf("Element %d found at index %d\n", searchNumber, foundCount)
+	} else {
+		fmt.Printf("Element %d not found in the array\n", searchNumber)
+	}
+}
+
+// Iterative Implementation
+func binarySearchIterative(arr []int, searchNumber int, zero int, one int) int {
+	var i int
+	var length = len(arr)
+	var mid int
 	for i = 0; i < length; i++ {
 		mid = (zero + one) / 2
-		if searchNumber < providedArray[mid] {
+		if searchNumber < arr[mid] {
 			one = mid
-		} else if searchNumber == providedArray[mid] {
-			fmt.Printf("Provided number found @ position %d\n", mid)
-			foundCount++
+		} else if searchNumber == arr[mid] {
 			break
 		} else {
 			zero = mid
 		}
 	}
 
-	if foundCount == 0 {
-		fmt.Println("Provided number not present in the array!")
+	return mid
+}
+
+// Recursive Implementation
+func binarySearchRecursive(arr []int, searchNumber int, zero int, one int) int {
+	if zero > one {
+		return -1
+	}
+
+	mid := (zero + one) / 2
+
+	if arr[mid] < searchNumber {
+		return binarySearchRecursive(arr, searchNumber, mid+1, one)
+	} else if arr[mid] == searchNumber {
+		return mid
+	} else {
+		return binarySearchRecursive(arr, searchNumber, zero, mid-1)
 	}
 }
